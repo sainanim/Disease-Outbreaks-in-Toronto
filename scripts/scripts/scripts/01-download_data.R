d0325ef755fba319b113a18104b8628bc95ab3cf
sainanim <156145306+sainanim@users.noreply.github.com>
2024-01-25 00:41:54 -0500

    Create 01-download_data.R

---
+++ scripts/scripts/scripts/01-download_data.R
@@ -0,0 +1,21 @@
+#### Preamble ####
+# Purpose: Downloads and saves the data from opendatatoronto
+# Author: Monica Sainani
+# Date: 24 January 2024
+# Contact: monica.sainani@mail.utoronto.ca 
+# License: MIT
+
+#### Workspace setup ####
+# install.packages("tidyverse")
+# install.packages("opendatatoronto")
+# install.packages("arrow")
+
+library(opendatatoronto)
+library(tidyverse)
+library(arrow)
+
+#### Download data ####
+Disease_data <- list_package_resources("80ce0bd7-adb2-4568-b9d7-712f6ba38e4e")|>get_resource()
+
+#### Save data as Parquet ####
+write_parquet(Disease_data, "inputs/data/unedited_data.parquet")
