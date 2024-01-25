b60ce4a2867429c73d7e4ef7cb087e395764ac5f
sainanim <156145306+sainanim@users.noreply.github.com>
2024-01-25 08:27:14 -0500

    Update 01-download_data.R

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -15,7 +15,8 @@ library(tidyverse)
 library(arrow)
 
 #### Download data ####
-Disease_data <- list_package_resources("80ce0bd7-adb2-4568-b9d7-712f6ba38e4e")|>get_resource()
+Disease_data <- list_package_resources("80ce0bd7-adb2-4568-b9d7-712f6ba38e4e") |> filter(name == "ob_report_2024.csv") |>
+get_resource()
 
 #### Save data as Parquet ####
 write_parquet(Disease_data, "inputs/data/unedited_data.parquet")
